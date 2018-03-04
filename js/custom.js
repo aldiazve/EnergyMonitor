663fb9b7acae7bf2f95ae237b915db1b2b420a24
Daniel Santos <dfsantosbu@unal.edu.co>
2018-03-03 19:26:29 -0500

    Improve margins.

--- js/custom.js
+++ js/custom.js
@@ -8,3 +8,7 @@
  function displayOptions(){
   document.getElementById("sub-progress").style.display = "inline";
  }
+
+ function displayRightMenu(){
+  document.getElementById("sidenav-right").style.display = "inline";
+ }
